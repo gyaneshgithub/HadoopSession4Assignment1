55ef11e94334aa491af2af7d029498cc08a6e731
gyaneshgithub <32609252+gyaneshgithub@users.noreply.github.com>
2017-11-22 21:50:26 +0530

    Add files via upload

---
+++ acad.java
@@ -0,0 +1,14 @@
+package assignment;
+
+public class acad{
+
+	public static void main(String[] args) {
+		int a;
+		int b;
+		int c;
+		a = 14;
+		b = 11;
+		c = a + b;
+		System.out.println("sum is : " +  c );
+	}     
+}
